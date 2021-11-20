f3f7467fa25cb73d3e627377a5bbfd5478bf40df
“lw20000” <1291958264@qq.com>
2021-11-20 22:11:18 +0800

    提交新文件

---
+++ cloud-comsumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -0,0 +1,51 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+/**
+ * @Auther: weiliang
+ * @Date: 2021/11/20 18:16
+ * @Description:
+ */
+@RestController
+@RequestMapping("comsumer")
+public class OrderController {
+
+    public static final String PAYENT_URL = "http://localhost:8001";
+//    public static final String PAYENT_URL = "http://CLOUD-PAYMENT-SERVICE";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+//    @Resource
+//    private LoadBalaner loadBalaner;
+//    @Resource
+//    private DiscoveryClient discoveryClient;
+
+    @GetMapping("payment/create")
+    public CommonResult<Payment> create(Payment payment) {
+        return restTemplate.postForObject(PAYENT_URL+"/payment/create",payment,CommonResult.class);
+    }
+    @GetMapping("payment/selectOne/{id}")
+    public CommonResult<Payment> getPayment(@PathVariable("id") long id) {
+        return restTemplate.getForObject(PAYENT_URL+"/payment/selectOne/"+id,CommonResult.class);
+    }
+
+//    @GetMapping("payment/lb")
+//    public String getPaymentLB() {
+//        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+//        if (instances == null || instances.size() == 0) {
+//            return null;
+//        }
+//        ServiceInstance serviceInstance = loadBalaner.instance(instances);
+//        URI uri = serviceInstance.getUri();
+//        System.out.println(uri+"/payment/lb");
+//        return restTemplate.getForObject(uri+"/payment/lb",String.class);
+////        return uri+"/payment/lb";
+//    }
+}
